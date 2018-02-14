eaf7975f62edc430ca902586103e74bb80f97ea0
Daniel Murray <danielmurray@airbears2-10-142-172-54.airbears2.1918.berkeley.edu>
2018-02-14 12:37:45 -0800

    workingapi

--- userSearch.js
+++ userSearch.js
@@ -3,19 +3,26 @@ var fs = require("fs");
 var UserSearch = function(name, location){
 	this.name = name;
 	this.location = location;
+	this.date = Date.now();
+	this.sendData = function(){
+		var newUser = UserSearch.name + UserSearch.location;
 
-}
-var newUser = UserSearch.name + UserSearch.location;
+		fs.appendFile(textFile, newUser, function(err){
 
-fs.appendFile(textFile, newUser, function(err){
-
-	if (err){
-		console.log(err);
-	}
-	else{
-		console.log("Content Added!");
+		if (err){
+			console.log(err);
+		}
+		else{
+			console.log("Data Sent");
 	}
 });
+
+	}
+
+};
+
+
+
 
 
 module.exports = UserSearch;
--- weather-.js
+++ weather-.js
@@ -1,13 +1,15 @@
-var Student = require("./userSearch.js");
+var newUser = require("./userSearch.js");
+console.log(newUser)
 
 
 var weather = require('weather-js');
- 
+
+// var test = new newUser("frank", "Hayward CA")
 // Options: 
 // search:     location name or zipcode 
 // degreeType: F or C 
  
-weather.find({search: UserSearch.location, degreeType: 'F'}, function(err, result) {
+weather.find({search: process.argv[2] + process.argv[3], degreeType: 'F'}, function(err, result) {
   if(err) console.log(err);
  
   console.log(JSON.stringify(result, null, 2));
